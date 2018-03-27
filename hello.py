680d5b5b3fff557025db8a0a77396c8fb0b659a2
zhangcookie <nogood007@outlook.com>
2018-03-27 22:52:27 +0800

    增加了对数据库的操作

--- hello.py
+++ hello.py
@@ -12,12 +12,39 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 from wtforms import StringField, SubmitField
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "what is this"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.split")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship("User", backref="role")
+
+    def __repr__(self):
+        return "<Role %r>" % self.name
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+
+    def __repr__(self):
+        return "<User %r>" % self.username
 
 
 class NameForm(FlaskForm):
@@ -27,24 +54,25 @@ class NameForm(FlaskForm):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # name = None
     form = NameForm()
-    # print("----------", form)
     if form.validate_on_submit():
-        print(form.name)
-        old_name = session.get("name")
-        if old_name is not None and old_name != form.name.data:
-            flash("Looks like you have changed your name!")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user:
+            session["known"] = True
+        else:
+            user_role = Role(name=form.name.data)
+            users = User(username=form.name.data, role=user_role)
+            db.session.add_all([users, user_role])
+            db.session.commit()
+            session["known"] = False
+        # old_name = session.get("name")
+        # if old_name is not None and old_name != form.name.data:
+        #     flash("Looks like you have changed your name!")
         session["name"] = form.name.data  # 将请求的数据存储到用户会话中
-        # form.name.data = ""
+        form.name.data = ""
         return redirect(url_for("index"))  # 使用重定向和url生成函数
-    return render_template("index.html", form=form, name=session.get("name"))
+    return render_template("index.html", form=form, name=session.get("name"), known=session.get("known", False))
 
-
-# @app.route("/usr/<id>")
-# def get_usr(id):
-#     usr = load_user(id)
-# return "<h1> hello %s </h1>" % name
 
 @app.route("/user/<name>")
 def user(name=None):
